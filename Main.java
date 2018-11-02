ed9cf83fd17fa65b8e71d521795768b213f68d0f
Winz <mawin_07@hotmail.com>
2018-11-02 13:09:07 +0700

    Finish about Product, get and get all

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+//        ArrayList<Product> product = MainpageService.showAllProduct();
+//        for(Product e : product)
+//            System.out.println(e);
+
+//        Product product = MainpageService.getProduct("S01");
+//        System.out.println(product);
+    }
+}
---
+++ Product.java
@@ -0,0 +1,71 @@
+public class Product {
+    private String id;
+    //private byte picture;
+    private String name;
+    private int price;
+    private String size;
+    private String color;
+    private String brand;
+    //private int num;
+
+    Product(String id, String name, int price, String size, String color, String brand){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.size = size;
+        this.color = color;
+        this.brand = brand;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String toString(){
+        return id + " " + name + " " + price + " " + size + " " + color;
+    }
+}
