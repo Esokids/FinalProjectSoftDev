484e625258a2e25ba07a28b4d9d36aaa3c874e2c
Winz <mawin_07@hotmail.com>
2018-12-14 18:50:10 +0700

    I'm so Tired !.

--- LoginService.java
+++ LoginService.java
@@ -1,2 +1,20 @@
 public class LoginService {
+
+    public static boolean checkUsername(User user){
+        User thisUser = null;
+        thisUser = UserService.getUser(user.getUsername());
+        if(thisUser == null)
+            return false;
+        else
+            return  true;
+    }
+
+    public static boolean checkPassword(User user){
+        User thisUser = null;
+        thisUser = UserService.getUser(user.getUsername());
+        if(thisUser.getPassword().equals(user.getPassword()))
+            return true;
+        else
+            return false;
+    }
 }
